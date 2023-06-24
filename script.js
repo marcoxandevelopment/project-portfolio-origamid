4679890e2c3995d17dd1727071d3b56005562c22
Marco Antônio "xan <129339274+marcoxandevelopment@users.noreply.github.com>
2023-06-23 23:03:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+ScrollReveal({
+  reset: true,
+  distance: "200px",
+  duration: 1000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".content-main-text", { delay: 800 });
+ScrollReveal().reveal(".content-main-fleximg", { delay: 200 });
+ScrollReveal().reveal(".content-experiencia-h1", {
+  delay: 200,
+  origin: "left",
+});
+ScrollReveal().reveal(".content-experiencia-text", {
+  delay: 1000,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".section-experience", { delay: 200, origin: "left" });
+ScrollReveal().reveal(".container-image-form", {
+  delay: 200,
+  origin: "bottom",
+});
+ScrollReveal().reveal(".content-form-flexbox", {
+  delay: 1000,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".container-contact-book", {
+  delay: 1000,
+  origin: "right",
+});
